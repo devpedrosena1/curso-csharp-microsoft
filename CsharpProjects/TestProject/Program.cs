ae3e40471b35b45417a690e468127e079fda6fe4
devpedrosena1 <devpedrosena1@gmail.com>
2025-08-11 19:38:54 -0300

    entendendo como funcionam metodos em c#

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C#!");
+﻿//See https://aka.ms/new-console-template for more information
+
+// Random dice = new();
+// int roll = dice.Next(1, 7);
+// Console.WriteLine(roll);
+
+// int roll1 = dice.Next();
+// int roll2 = dice.Next(101);
+// int roll3 = dice.Next(50, 101);
+
+// Console.WriteLine($"First roll: {roll1}");
+// Console.WriteLine($"Second roll: {roll2}");
+// Console.WriteLine($"Third roll: {roll3}");
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+
+largerValue = System.Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
--- CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bbb8f12c750003e4213ab9dae9cd38acc2e748f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fe7301bbacf93ca879a54f13afc8e91c67416e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
