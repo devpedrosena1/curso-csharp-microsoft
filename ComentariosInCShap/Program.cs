e8c7631a202f620fdacfa34f7345a14b9db76de0
devpedrosena1 <devpedrosena1@gmail.com>
2025-08-12 14:42:23 -0300

    refatorando código e comentando instruções

--- ComentariosInCShap/Program.cs
+++ ComentariosInCShap/Program.cs
@@ -25,3 +25,36 @@ foreach (var orderID in orderIDs)
 {
     Console.WriteLine(orderID);
 }
+
+Console.WriteLine("\n=== Exercise ===");
+
+/*
+   This code reverses a message, counts the number of times
+   a particular character appears, then prints the results
+   to the console window.
+ */
+
+string str = "The quick brown fox jumps over the lazy dog.";
+
+// convert the message into a char array
+char[] charMessage = str.ToCharArray();
+// Reverse the chars
+Array.Reverse(charMessage);
+
+int letterCount = 0;
+
+// count the o's
+foreach (char i in charMessage)
+{
+    if (i == 'o')
+    {
+        letterCount++;
+    }
+}
+
+// convert it back to a string
+string new_message = new String(charMessage);
+
+// print it out
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {letterCount} times.");
