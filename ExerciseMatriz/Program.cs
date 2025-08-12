7564c92cfc4803dc2bf1b22414a6db2ecaad15ec
devpedrosena1 <devpedrosena1@gmail.com>
2025-08-12 14:35:00 -0300

    /*

---
+++ ExerciseMatriz/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+// criar a matriz
+string[] fraudulentIds = [ "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" ];
+
+foreach (var ids in fraudulentIds)
+{
+    if (ids.StartsWith("B"))
+    {
+        Console.WriteLine(ids);
+    }
+}
